10c266d6346751a38020cb69eed37dde39e3b9ad
juan <seco007@hotmail.com>
2017-12-10 14:52:04 -0600

    Login controller...

---
+++ app/controllers/login.js
@@ -0,0 +1,11 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+  actions: {
+    login(username, password) {
+    this.set('currentuser', user);
+  }
+     else {
+     return ('Wrong username and/or password')
+     }
+   },
